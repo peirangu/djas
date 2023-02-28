1ecb87be5c3769efc5426d0a8dcca8fa7694a666
gupeiran <1393136809@qq.com>
2023-02-28 16:46:02 +0800

    test

---
+++ _3_27test/demo/src/com/test/test.java
@@ -0,0 +1,8 @@
+package test;
+
+public class test {
+    public static void main(String[] das) {
+        System.out.println("hello");
+        student student = new student;
+    }
+}
---
+++ test/test1/src/com/test1/tets.java
@@ -0,0 +1,2 @@
+package com.test1;public class tets {
+}
